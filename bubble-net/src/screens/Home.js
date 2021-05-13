119c3f7a9a344ceb60a60936f994ac0ebe70a841
jyeung28 <83853858+jyeung28@users.noreply.github.com>
2021-05-13 14:47:08 -0700

    Revert "added home, sign up, and log in screen"

--- bubble-net/src/screens/Home.js
+++ bubble-net/src/screens/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+// import Button from "react-native-button";
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 import { withNavigation } from 'react-navigation';
 // Firebase App (the core Firebase SDK) is always required and must be listed first
@@ -42,103 +42,33 @@ class Home extends React.Component {
     render() {
 
       return (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.container} onPress={() => {this.props.navigation.navigate('LogIn')}}>
-            <Image style={styles.logo} 
-              source={require('../assets/logo.png')}/>
-        </TouchableOpacity>
-
-      </View>
+      <TouchableOpacity style={styles.container} /*onPress={() => {this.props.navigation.navigate('Login')}}*/>
+          <Image style={styles.logo} 
+            source={require('../assets/logo.png')}/>
+      </TouchableOpacity>
     );
   }   
 };
 
-// Must export with "withNavigation" to connect to navigator
 export default withNavigation(Home);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF5E0',
+    backgroundColor: 'white',
     alignItems: 'center',
     marginTop: '0%'
   },
-  welcomeMessage: {
-    fontFamily: "gotham_rounded_medium",
-    fontSize: 16,
-    color: "#3C3C4385",
-    alignItems: 'center',
-    fontWeight: "bold",
-    justifyContent: 'center',
-    textAlign: 'center',
-    marginBottom: 30,
-    width: "80%"
-  },
   logo: {
-    width: 220,
-    height: 220,
-    marginBottom: 20,
-    marginTop: '50%'
+  	width: 300,
+  	height: 300,
+  	marginBottom: 20,
+    marginTop: '60%'
   },
-  inputView: {
-    width: "80%",
-    backgroundColor: "#EDEDED",
-    borderColor: "#4BBFC370",
-    borderRadius: 5,
-    borderBottomWidth: 1,
-    borderTopWidth: 1,
-    borderLeftWidth: 1,
-    borderRightWidth: 1, 
-    shadowColor: "#18A3A7",
-    shadowRadius: 0, 
-    shadowOpacity: 10,
-    shadowOffset: {
-      width: 0,
-      height: 1.5
-    },
-    height: 55,
-    marginBottom: 20,
-    justifyContent: "center",
-    padding: 20
-
-  },
-  inputText: {
-    fontFamily: "gotham_rounded_book",
-    height: 60,
-    color: "#3C3C43"
-  },
-  forgot: {
-    fontFamily: "gotham_rounded_book",
+  fieldTrip: {
+    fontWeight: "bold",
+    fontSize: 45,
     color: "#18A3A7",
-    fontSize: 14,
-    position: "relative",
-    paddingLeft: 210
-  },
-  createAccount: {
-    fontFamily: "gotham_rounded_book",
-    color: "#18A3A7",
-    fontSize: 14,
-  },
-  loginBtn: {
-    width: "80%",
-    backgroundColor: "#4BBFC3",
-    borderRadius: 5,
-    height: 40,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    marginBottom: 10,
-    shadowColor: "#18A3A7",
-    shadowRadius: 0, 
-    shadowOpacity: 10,
-    shadowOffset: {
-      width: 0,
-      height: 1.5
-    },
-  },
-  loginText: {
-    fontFamily: "gotham_rounded_medium",
-    color: "white",
-    fontWeight: "bold"
+    marginBottom: 15
   }
 });
